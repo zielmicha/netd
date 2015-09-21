14c907cc896ee9df536d19c36e47c912a00a0f50
Michał Zieliński <michal@zielinscy.org.pl>
2015-09-21 09:07:19 +0200

    basic dbus interface

--- netd/dbuscore.nim
+++ netd/dbuscore.nim
@@ -1,21 +1,30 @@
-import dbus
+import dbus, dbus/def
 import netd/core
 
 type
   DbusCorePlugin* = ref object of Plugin
     bus*: Bus
+    netdObject*: DbusObjectImpl
 
 proc create*(t: typedesc[DbusCorePlugin], manager: NetworkManager): DbusCorePlugin =
   new(result)
   result.manager = manager
 
+proc Reload*(self: DbusCorePlugin) =
+  self.manager.reload()
 
-proc init*(plugin: DbusCorePlugin, bus: Bus) =
+let coreDef = newInterfaceDef(DbusCorePlugin)
+coreDef.addMethod(Reload, [], [])
+
+proc initCoreInterface*(self: DbusCorePlugin) =
+  self.netdObject.addInterface("net.networkos.netd.Core", coreDef, self)
+
+proc init*(self: DbusCorePlugin, bus: Bus) =
+  self.bus = bus
+  self.netdObject = newObjectImpl(bus)
+
+  initCoreInterface(self)
+  enableIntrospection(self.netdObject)
+
   bus.requestName("net.networkos.netd")
-
-  # TODO
-  proc callback(kind: IncomingMessageType, messsage: IncomingMessage): bool =
-    echo kind, " ", messsage.name, " ", messsage.interfaceName
-    return false
-
-  bus.registerObject("/net/networkos/netd".ObjectPath, callback)
+  bus.registerObject("/net/networkos/netd".ObjectPath, self.netdObject)
